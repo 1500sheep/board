0ddc79a49eadff1c1684c0f8f82d14bfcfc7b5c4
1500sheep <cjswotl6274@naver.com>
2019-01-09 11:44:57 +0900

    apply messageSource, LoginUserHandlerMethodArgumentResolver

--- src/main/java/com/board/config/WebMvcConfig.java
+++ src/main/java/com/board/config/WebMvcConfig.java
@@ -1,15 +1,40 @@
 package com.board.config;
 
+import com.board.security.LoginUserHandlerMethodArgumentResolver;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(30);
+        return messageSource;
+    }
+
+    @Bean
+    public MessageSourceAccessor messageSourceAccessor(MessageSource messageSource) {
+        return new MessageSourceAccessor(messageSource);
+    }
+
+    @Bean
+    public LoginUserHandlerMethodArgumentResolver loginUserArgumentResolver() {
+        return new LoginUserHandlerMethodArgumentResolver();
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver());
     }
 }
