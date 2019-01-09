e2b736a170cc735054b2149356f638a7942355fe
1500sheep <cjswotl6274@naver.com>
2019-01-09 11:42:18 +0900

    apply spring security , but not completed (#10)

---
+++ src/main/java/com/board/config/ApplicationInitializer.java
@@ -0,0 +1,21 @@
+//package com.board.config;
+//
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+//import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+//
+//public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+//    @Override
+//    protected Class<?>[] getRootConfigClasses() {
+//        return new Class[]{WebSecurityConfiguration.class};
+//    }
+//
+//    @Override
+//    protected Class<?>[] getServletConfigClasses() {
+//        return new Class[0];
+//    }
+//
+//    @Override
+//    protected String[] getServletMappings() {
+//        return new String[0];
+//    }
+//}
