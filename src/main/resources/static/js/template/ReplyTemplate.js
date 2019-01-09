6a013717bb78c3616146f5f8856d9133351ac80a
1500sheep <cjswotl6274@naver.com>
2019-01-09 11:29:52 +0900

    create template about reply (#22)

---
+++ src/main/resources/static/js/template/ReplyTemplate.js
@@ -0,0 +1,32 @@
+export function registerReplyTemplate() {
+    return `
+        <div class="panel-body">
+            <form id="reply-form" method="post">
+                <fieldset>
+                    <div class="form-group">
+                        <textarea class="form-control comment" rows="3" placeholder="Write in your wall" autofocus=""></textarea>                            
+                    </div>
+                    <!--<button type="submit" class="[ btn btn-info]" data-loading-text="Loading...">댓글 등록</button>-->
+                </fieldset>
+            </form>
+        </div>
+    `;
+}
+
+export function replyTemplate({writer, comment}) {
+    const profileUrl = writer.profile ? writer.profile.fileUrl : '';
+
+    return `<li class='post-reply'>
+               <div class='post-reply-wrap'>
+                   <div class='post-reply-profile'>
+                   <img alt='${writer.name}-profile' src='${profileUrl}'>
+                   </div>
+                   <div class='post-reply-name'>
+                       <p>${writer.name}</p>
+                   </div>
+    
+                   <div class='post-reply-text'>${comment}</div>
+                   
+               </div>
+           </li>`;
+}
